c13c9cac366d4be349729b8ea9f4843b142e5ad0
anaskhamees <anaskhamees65@gmail.com>
2024-03-07 06:23:01 +0200

    add comments

--- src/MCAL/NVIC/NVIC.h
+++ src/MCAL/NVIC/NVIC.h
@@ -69,14 +69,69 @@ typedef struct
 /************************************************APIs****************************************************/
 /********************************************************************************************************/
 
+/**
+  * @brief  Enables a specific IRQ.
+  * @param  IRQ: The IRQ number to be enabled.
+  * @retval ErrorStatus_t: Indicates the status of the function.
+  */
 ErrorStatus_t NVIC_EnableIRQ(uint8_t IRQ);
+/**
+  * @brief  Disables a specific IRQ.
+  * @param  IRQ: The IRQ number to be disabled.
+  * @retval ErrorStatus_t: Indicates the status of the function.
+  */
 ErrorStatus_t NVIC_DisableIRQ(uint8_t IRQ);
+/**
+  * @brief  Sets a pending state for a specific IRQ.
+  * @param  IRQ: The IRQ number to set its pending state.
+  * @retval ErrorStatus_t: Indicates the status of the function.
+  */
 ErrorStatus_t NVIC_SetPendingIRQ(uint8_t IRQ);
+/**
+  * @brief  Clears a pending state for a specific IRQ.
+  * @param  IRQ: The IRQ number to clear its pending state.
+  * @retval ErrorStatus_t: Indicates the status of the function.
+  */
 ErrorStatus_t NVIC_ClearPendingIRQ(uint8_t IRQ);
+/**
+  * @brief  Gets the pending state of a specific IRQ.
+  * @param  IRQ: The IRQ number to get its pending state.
+  * @param  PendingState: Pointer to where the pending state will be stored.
+  * @retval ErrorStatus_t: Indicates the status of the function.
+  */
 ErrorStatus_t NVIC_GetPendingIRQ(uint8_t IRQ, uint8_t *PendingState);
+/**
+  * @brief  Gets the active state of a specific IRQ.
+  * @param  IRQ: The IRQ number to get its active state.
+  * @param  ActiveState: Pointer to where the active state will be stored.
+  * @retval ErrorStatus_t: Indicates the status of the function.
+  */
 ErrorStatus_t NVIC_GetActiveState(uint8_t IRQ, uint8_t *ActiveState);
-ErrorStatus_t NVIC_SetPrioirityGrouping(uint8_t IRQ,uint32_t PriorityGrouping,uint8_t Preemption,uint8_t SubGroup);
-ErrorStatus_t NVIC_GetPriority(uint8_t IRQ,uint32_t Priority);
+/**
+  * @brief  Sets the priority for a given interrupt request (IRQ).
+  * @param  IRQ: The IRQ number for which the priority is to be set.
+  * @param  PriorityGrouping: The priority grouping configuration value.
+  *              This value determines the number of bits used for preemption and subgroup priority.
+  *              It should be one of the values:
+  *              - NVIC_PREEMPTION_16_SUBGROUP_NONE
+  *              - NVIC_PREEMPTION_8_SUBGROUP_TWO
+  *              - NVIC_PREEMPTION_4_SUBGROUP_FOUR
+  *              - NVIC_PREEMPTION_2_SUBGROUP_EIGHT
+  *              - NVIC_PREEMPTION_NONE_SUBGROUP_SIXTEEN
+  * 
+  * @param  Preemption: The preemption priority value for the IRQ.
+  *              This value specifies the priority of the IRQ within its preemption group.
+  *              The range of valid values is determined by the number of bits allocated(By PriorityGrouping) for preemption priority.
+  * @param  SubGroup: The subgroup priority value for the IRQ.
+  *              This value specifies the priority of the IRQ within its subgroup.
+  *              The range of valid values is determined by the number of bits (By PriorityGrouping) allocated for subgroup priority.
+  * @retval ErrorStatus_t: Indicates the status of the function execution.
+  *              - NVIC_OK: The operation was successful.
+  *              - NVIC_WRONG_PARAMETER: One or more parameters are invalid.
+  */
+ErrorStatus_t NVIC_SetPriority(uint8_t IRQ, uint32_t PriorityGrouping, uint8_t Preemption, uint8_t SubGroup)
+
+//ErrorStatus_t NVIC_GetPriority(uint8_t IRQ,uint32_t Priority);
 
 
 
